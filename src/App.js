d06068ba59096faeadd8e65af4a38f4de6543bc8
yermakova-l <lyudmyla.yermakova@gmail.com>
2019-12-06 20:49:19 +0200

    Creating ShopPage and CollectionPreview

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Switch, Route, Link } from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <Link to='/'>Home Page LINK</Link>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -16,7 +10,8 @@ function App() {
       
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
+        
       </Switch>
      
     </div>
---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './collection-preview.styles.scss';
+
+const CollectionPreview = ({title, items}) => (
+    <div >
+        <h1>{title.toUpperCase()}</h1>
+        <div>
+        {
+             items.map((item) => (
+                 <div key={item.id}>{item.name}</div>
+             ))}
+        </div>
+    </div>
+);
+export default CollectionPreview;
